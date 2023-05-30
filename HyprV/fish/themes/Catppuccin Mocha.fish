f3da4e2b7df47656cc1ef0469998e1e37f1584b0
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2023-05-30 13:57:51 +0100

    add theme change

---
+++ HyprV/fish/themes/Catppuccin Mocha.fish
@@ -0,0 +1,30 @@
+# name: 'Catppuccin mocha'
+# url: 'https://github.com/catppuccin/fish'
+# preferred_background: 1e1e2e
+
+set -Ux fish_color_normal cdd6f4
+set -Ux fish_color_command 89b4fa
+set -Ux fish_color_param f2cdcd
+set -Ux fish_color_keyword f38ba8
+set -Ux fish_color_quote a6e3a1
+set -Ux fish_color_redirection f5c2e7
+set -Ux fish_color_end fab387
+set -Ux fish_color_comment 7f849c
+set -Ux fish_color_error f38ba8
+set -Ux fish_color_gray 6c7086
+set -Ux fish_color_selection --background=313244
+set -Ux fish_color_search_match --background=313244
+set -Ux fish_color_option a6e3a1
+set -Ux fish_color_operator f5c2e7
+set -Ux fish_color_escape eba0ac
+set -Ux fish_color_autosuggestion 6c7086
+set -Ux fish_color_cancel f38ba8
+set -Ux fish_color_cwd f9e2af
+set -Ux fish_color_user 94e2d5
+set -Ux fish_color_host 89b4fa
+set -Ux fish_color_host_remote a6e3a1
+set -Ux fish_color_status f38ba8
+set -Ux fish_pager_color_progress 6c7086
+set -Ux fish_pager_color_prefix f5c2e7
+set -Ux fish_pager_color_completion cdd6f4
+set -Ux fish_pager_color_description 6c7086
